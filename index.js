70177a587a514efc37b8106697ce8a4aacac9dd1
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-11-28 08:56:17 +0600

    Added MongoDB & Connect Database

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
-// var cors = require('cors')
+require('dotenv').config()
+var cors = require('cors')
+app.use(cors())
+app.use(express.json());
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4oearpf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("user-auth").collection("user")
+    app.get('/users', async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
